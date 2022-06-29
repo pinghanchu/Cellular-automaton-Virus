276dfc849fcbdbbc514177e97dee8a8fa3433112
Pinghan Chu <pinghan@gmail.com>
2022-06-29 15:14:27 -0600

    application update

--- submit.py
+++ submit.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 import os
 ifnprods = [0,1,3,6,10]
-isstochastic=[[0,280],[1,2000]]
-initial_virus = [[1,1,1],[20,20,10],[20,20,100],[200,500,10],[200,500,100]]
-for Is,totalstep in isstochastic:
+
+isstochastic=[["homogenous",0,280]]
+#initial_virus = [[1,1,1],[20,20,10],[20,20,100],[200,500,10],[200,500,100]]
+#initial_virus = [[1,1,1]]
+initial_virus = [[20,20,10]]
+for name,Is,totalstep in isstochastic:
 	for s1,s2,vnumb in initial_virus:
 		for iifnprods in ifnprods:
-			path = "./initial_virus/{}/{}_{}_{}/{}/".format(Is,s1,s2,vnumb,iifnprods)
+			path = "./initial_virus/{}/{}_{}_{}/{}/".format(name,s1,s2,vnumb,iifnprods)
 			os.chdir(path)
-			os.system("python COVID.py {} {} {} {} {} {}".format(iifnprods,s1,s2,vnumb,Is,totalstep)
+			os.system("python COVID.py {} {} {} {} {} {}&".format(iifnprods,s1,s2,vnumb,Is,totalstep))
 			os.chdir("../../../../")
